62d3016a91da0f0687efda0f1d2c1e3ab0f45eeb
CharlyPico <mr.piico@gmail.com>
2024-06-29 08:34:51 -0600

    Initial Commit

---
+++ Country/ContentView.swift
@@ -0,0 +1,20 @@
+//
+//  ContentView.swift
+//  Country
+//
+//  Created by Charlie Pico on 29/06/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
---
+++ Country/CountryApp.swift
@@ -0,0 +1,17 @@
+//
+//  CountryApp.swift
+//  Country
+//
+//  Created by Charlie Pico on 29/06/24.
+//
+
+import SwiftUI
+
+@main
+struct CountryApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
