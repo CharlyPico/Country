cf9d9256ab7ccf918e2657192bea89dc64732593
CharlyPico <mr.piico@gmail.com>
2024-07-01 16:00:43 -0600

    Today the ColorInfo file was deleted as it wasn't necessary. Some test were made and some optimizations where applied.

---
+++ Country/COUNTRY DETAIL/CountryNotesItems.swift
@@ -0,0 +1,18 @@
+//
+//  CountryNotesItems.swift
+//  Country
+//
+//  Created by Charlie Pico on 01/07/24.
+//
+
+import SwiftUI
+
+struct CountryNotesItems: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    CountryNotesItems()
+}
